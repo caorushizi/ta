f7b95f43f89541e5b91b7f87c8785efa9ce15364
caorushizi <84996057@qq.com>
2020-05-10 23:00:43 +0800

    Initial Commit.

--- lib/bubble.dart
+++ lib/bubble.dart
@@ -1,22 +1,63 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class BubbleWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+class Toast {
+  static OverlayEntry _overlayEntry; // toast靠它加到屏幕上
+  static bool _showing = false; // toast是否正在showing
+  static DateTime _startedTime; // 开启一个新toast的当前时间，用于对比是否已经展示了足够时间
+  static String _text; // 提示内容
+
+  static void toast(BuildContext context, String text) async {
+    _text = text;
+    _startedTime = DateTime.now();
+    //获取OverlayState
+    OverlayState overlayState = Overlay.of(context);
+    _showing = true;
+    if (_overlayEntry == null) {
+      _overlayEntry = OverlayEntry(
+        builder: (BuildContext context) => Positioned(
+          top: 300,
+          child: Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width - 100,
+            margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
+            child: AnimatedOpacity(
+              opacity: _showing ? 1.0 : 0.0, //目标透明度
+              duration: Duration(milliseconds: 400),
+              child: _buildToastWidget(),
+            ),
+          ),
+        ),
+      );
+      overlayState.insert(_overlayEntry);
+    } else {
+      //重新绘制UI，类似setState
+      _overlayEntry.markNeedsBuild();
+    }
+
+    await Future.delayed(Duration(milliseconds: 2000)); // 等待时间
+    //2秒后 到底消失不消失
+    if (DateTime.now().difference(_startedTime).inMilliseconds >= 2000) {
+      _showing = false;
+      _overlayEntry.markNeedsBuild();
+      await Future.delayed(Duration(milliseconds: 400));
+      _overlayEntry.remove();
+      _overlayEntry = null;
+    }
+  }
+
+  //toast绘制
+  static _buildToastWidget() {
     return Container(
-      height: 20,
-      alignment: Alignment.center,
       child: Padding(
-        padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+        padding: EdgeInsets.fromLTRB(15.0, 5.0, 15.0, 5.0),
         child: Text(
-          "你好世界",
+          _text,
           style: TextStyle(
             decoration: TextDecoration.none,
-//          color: Color(0x8CB4D6FF),
-            color: Colors.red,
-            fontSize: 10,
+            color: Color(0xFFB4D6FF),
+            fontSize: 15,
             fontFamily: "KaiTi",
-//          height: 20,
           ),
         ),
       ),
@@ -26,7 +67,7 @@ class BubbleWidget extends StatelessWidget {
           width: 1,
         ),
         borderRadius: BorderRadius.all(
-          Radius.circular(7.0),
+          Radius.circular(10.0),
         ),
         color: Colors.white,
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -32,11 +33,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  final _remembranceDay = new DateTime(2018, 1, 28);
+  final _remembranceDay = DateTime(2018, 1, 28);
+  final _feedbackType = FeedbackType.light;
+  final arr = [
+    "饿的时候总想偷偷看你一眼，大概是因为秀色可餐吧。",
+    "我想变得有趣，变得特别，变成你眼里的一点星光。",
+    "如果可以和你在一起，我宁愿让天空所有的星光全部陨落，因为你的眼睛，是我生命里最亮的光芒。",
+    "我好喜欢你，从天南到地北，从地老到天荒。",
+    "有些东西，并不是越浓越好，要恰到好处。深深的话，我们浅浅地说，长长的路，我们慢慢地走。",
+    "写你名字可真难，倒不是笔画繁琐，只是写你名字时地蘸上四分春风，三分月色，两分微醺，还有一分你的眉眼才好。",
+    "知道我和唐僧有什么区别吗？唐僧想取经，而我想娶你！",
+    "你是我最光明的秘密，也是我从不曾拥有的底气。",
+    "我不要这世间的繁华，你就是我的天下。此生有你，世间再无他。",
+    "你若粗茶淡饭，与我共苦同甘，我便披诚露胆，陪你海枯石烂。你若齐眉举案，给我嘘寒问暖，我便重惜轻怜，护你一世长安。",
+    "我一生所见的风景，都难及你眉眼微颤的万种风情。",
+  ];
   int ourTotal;
 
   calcDays() {
-    var today = new DateTime.now();
+    var today = DateTime.now();
     setState(() {
       ourTotal = today.difference(_remembranceDay).inDays;
     });
@@ -44,15 +59,14 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    var _feedbackType = FeedbackType.light;
     calcDays();
     Timer(Duration(seconds: 5), () {
       calcDays();
     });
-    return new DecoratedBox(
-      decoration: new BoxDecoration(
-        image: new DecorationImage(
-          image: new AssetImage('assets/images/background.gif'),
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/background.gif'),
         ),
       ),
       child: Stack(
@@ -65,10 +79,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             left: 11,
             top: 72,
             child: GestureDetector(
-              child: new Image(image: new AssetImage("assets/images/5.png")),
+              child: Image(image: AssetImage("assets/images/5.png")),
               onTap: () {
                 print(5);
-                Vibrate.feedback(_feedbackType);
+                _showTip();
               },
             ),
           ),
@@ -77,10 +91,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             left: 12,
             top: 150,
             child: GestureDetector(
-              child: new Image(image: new AssetImage("assets/images/4.png")),
+              child: Image(image: AssetImage("assets/images/4.png")),
               onTap: () {
                 print(4);
-                Vibrate.feedback(_feedbackType);
+                _showTip();
               },
             ),
           ),
@@ -89,10 +103,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             left: 0,
             top: 397,
             child: GestureDetector(
-              child: new Image(image: new AssetImage("assets/images/1.png")),
+              child: Image(image: AssetImage("assets/images/1.png")),
               onTap: () {
                 print('1');
-                Vibrate.feedback(_feedbackType);
+                _showTip();
               },
             ),
           ),
@@ -101,10 +115,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             top: 472,
             left: 0,
             child: GestureDetector(
-              child: new Image(image: new AssetImage("assets/images/6.png")),
+              child: Image(image: AssetImage("assets/images/6.png")),
               onTap: () {
                 print(6);
-                Vibrate.feedback(_feedbackType);
+                _showTip();
               },
             ),
           ),
@@ -113,10 +127,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             top: 157,
             right: 0,
             child: GestureDetector(
-              child: new Image(image: new AssetImage("assets/images/3.png")),
+              child: Image(image: AssetImage("assets/images/3.png")),
               onTap: () {
                 print(3);
-                Vibrate.feedback(_feedbackType);
+                _showTip();
               },
             ),
           ),
@@ -125,10 +139,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             top: 284,
             right: 32,
             child: GestureDetector(
-              child: new Image(image: new AssetImage("assets/images/2.png")),
+              child: Image(image: AssetImage("assets/images/2.png")),
               onTap: () {
                 print(2);
-                Vibrate.feedback(_feedbackType);
+                _showTip();
               },
             ),
           ),
@@ -137,26 +151,92 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             right: 15,
             bottom: 15,
             child: GestureDetector(
-              child: new Image(
-                image: new AssetImage("assets/images/gift.png"),
+              child: Image(
+                image: AssetImage("assets/images/gift.png"),
                 fit: BoxFit.cover,
                 width: 50,
                 height: 50,
               ),
               onTap: () {
                 print('gift');
-                Vibrate.feedback(_feedbackType);
+                _showTip();
               },
             ),
           ),
           Positioned(
-            top: 100,
-            child: BubbleWidget(),
+            top: 120,
+            child: _buildBrand(),
           ),
           Positioned(
-            top: 145,
-            child: new Text(
-              "今天已经相恋 $ourTotal 天啦!",
+            top: 70,
+            child: Text(
+              "豆豆和莎莎",
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                color: Color(0xff4188C8),
+                fontSize: 20,
+                fontFamily: "KaiTi",
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _showTip() {
+    var s = arr[Random().nextInt(arr.length)];
+    // 手机震动提示
+    Vibrate.feedback(_feedbackType);
+    Toast.toast(context, s);
+  }
+
+  _buildBrand() {
+    return Container(
+      width: 294,
+      height: 64,
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            child: Image(
+              image: AssetImage("assets/images/brandBg.png"),
+              fit: BoxFit.cover,
+              width: 294,
+              height: 64,
+            ),
+          ),
+          Positioned(
+            top: 6,
+            left: 182,
+            child: Container(
+              width: 56,
+              height: 58,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/circle.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  "$ourTotal",
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    color: Color.fromARGB(255, 255, 165, 165),
+                    fontSize: 23,
+                    fontFamily: "KaiTi",
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 23,
+            left: 62,
+            child: Text(
+              "今天已经相恋",
               style: TextStyle(
                 decoration: TextDecoration.none,
                 color: Colors.white,
@@ -166,12 +246,13 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             ),
           ),
           Positioned(
-            top: 70,
-            child: new Text(
-              "豆豆和莎莎",
+            top: 23,
+            right: 5,
+            child: Text(
+              "天啦!",
               style: TextStyle(
                 decoration: TextDecoration.none,
-                color: Color(0xff4188C8),
+                color: Colors.white,
                 fontSize: 20,
                 fontFamily: "KaiTi",
               ),
